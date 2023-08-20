82e08181e57d9b352bfc3220312ba9672c68e468
Koye <54333700+kryptcode@users.noreply.github.com>
2023-08-20 23:13:56 +0100

    "sleepy time"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
+import About from "./components/About"
+import Header from "./components/Header"
+import Typewriter from 'typewriter-effect'
+import Projects from "./components/Projects"
+import 'animate.css'
 
 
 function App() {
 
   return (
-    <>
+    <div className="min-h-screen bg-black text-white overflow-x-hidden">
+      <Header />
 
-    </>
+      <div className="w-[90%] animate__animated animate__fadeInLeft mx-auto text-8xl lg:text-9xl oswald font-medium uppercase mb-32 lg:mb-64 ">
+        <div className="oswald">
+          HI, I&apos;M
+        </div>
+        <div className="oswald text-[#ffecd1]">
+          adekemi
+        </div>
+        <div className="text-2xl">
+        <Typewriter
+                options={{
+                    strings: ['Graphics Designer', 'Product Manager', 'technical writer'],
+                    autoStart: true,
+                    loop: true,
+        }}
+        />
+        </div>
+      </div>
+
+      <About />
+      <Projects />
+
+      {/* <div className="animate__animated animate__slideInLeft mb-32">
+        <div className="w-[90%] mx-auto py-10 ">
+            <h5 className=" text-5xl mb-5">Let&apos;s Collaborate</h5>
+            <a href="mailto:adeyombotk@gmail.com" className="w-48 h-48 rounded-full flex text-black justify-center items-center font-medium text-xl bg-[#ffecd1] ">
+              EMAIL ME
+            </a>
+        </div>
+      </div> */}
+
+      <div className="w-[90%] animate__animated animate__slideInLeft mx-auto py-10 text-3xl flex flex-col space-y-10 md:space-y-0 md:flex-row md:space-x-10 items-center text-[#ffecd1]">
+        <div >
+        <a href="https://www.behance.net/taiwoadeyombo" className=" py-1 px-4 border-[#ffecd1] border-[2px] ">BEHANCE</a>
+        </div>
+        <div>
+        <a href="mailto:adeyombotk@gmail.com" className=" py-1 px-4 border-[#ffecd1] border-[2px] ">adeyombotk@gmail.com</a>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Projects.jsx
@@ -0,0 +1,63 @@
+
+
+const Projects = () => {
+  return (
+    <div className="py-16">
+        <div className="w-[90%] mx-auto">
+          <h3 className="text-[#ffecd1] text-4xl font-medium ">
+            Work Archive 2020 - Present
+          </h3>
+
+          <div>
+            <div>
+              <img className="mx-auto" src="https://i.ibb.co/R31tH1D/Honeyview-AQUAMOTION.webp" alt="" loading="lazy" />
+            </div>
+
+            <div className="flex flex-col lg:flex-row justify-evenly space-x-2 my-6">
+              <div>
+                <img src="https://i.ibb.co/hFc3nNt/Honeyview-Doberman1-2.webp" alt="" />
+              </div>
+              <div>
+                <img src="https://i.ibb.co/qRF2ntG/Honeyview-Doberman2.webp" alt="" />
+              </div>
+              <div>
+                <img src="https://i.ibb.co/9T1fdPB/Honeyview-nusacredfeminine.webp" alt="" />
+              </div>
+            </div>
+
+            <div>
+              <img src="https://i.ibb.co/SJh1GB8/Honeyview-m-CD.webp" alt="" loading="lazy" />
+            </div>
+
+            <div className="flex flex-col lg:flex-row justify-evenly space-x-2 my-6">
+              <div>
+                <img src="https://i.ibb.co/zfMGdQJ/Honeyview-transmutationteemockup.webp" alt="" />
+              </div>
+              <div>
+                <img src="https://i.ibb.co/kcS6f6y/Honeyview-safespacewhite.webp" alt="" />
+              </div>
+              <div>
+                <img src="https://i.ibb.co/C9psM5Q/Honeyview-safespaceblacktee.webp" alt="" />
+              </div>
+            </div>
+
+            <div>
+              <img className="mx-auto" src="https://i.ibb.co/sQ5FH1V/Honeyview-SKK.webp" alt="" loading="lazy" />
+            </div>
+
+            <div className="flex flex-col lg:flex-row justify-evenly space-x-4 my-6">
+              <div>
+                <img src="https://i.ibb.co/fnfZ5Hq/Honeyview-nunicely.webp" alt="" />
+              </div>
+              <div>
+                <img src="https://i.ibb.co/ypdsb1J/Honeyview-Dobermann2black.webp" alt="" />
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Projects
